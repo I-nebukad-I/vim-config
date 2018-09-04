8c222fc915dc220dcc0b4c082b6d36d8a0c62d6e
Rene Raab <e1225391@student.tuwien.ac.at>
2018-09-04 21:32:14 +0200

    added vundle installation

--- ellipsis.sh
+++ ellipsis.sh
@@ -1,25 +1,32 @@
 #!/usr/bin/env bash
 
 pkg.install() {
+	# install vim
 	PKG_OK=$(dpkg-query -W --showformat='${Status}\n' vim|grep "install ok installed")
 	if [ "" == "$PKG_OK" ]; then
   		sudo apt-get --force-yes --yes install vim
 	fi
+
+	# install vundle
+	git.clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+	
+	# install plugins
+	vim +PluginInstall +qall
 }
 
 pkg.link() {
 	fs.link_rfile vimrc $HOME/.vimrc
-    
-	vim +PluginInstall +qall
 }
 
 pkg.uninstall() {
+	# remove vim 
 	PKG_OK=$(dpkg-query -W --showformat='${Status}\n' vim|grep "install ok installed")
 	if [ ! "" == "$PKG_OK" ]; then
   		sudo apt-get autoremove --purge vim 
-    fi
+	fi
 
-    rm -rf $HOME/.vim
+	# remove vim folder
+    	rm -rf $HOME/.vim
 }
 
 pkg.unlink() {
