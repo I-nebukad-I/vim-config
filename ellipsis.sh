2c48b6375e1a31a48f2025cc94bd52d8ba4c2a10
Rene Raab <e1225391@student.tuwien.ac.at>
2018-09-05 20:03:52 +0200

    added YouCompleteMe dependencies and installation

--- ellipsis.sh
+++ ellipsis.sh
@@ -7,16 +7,38 @@ pkg.install() {
   		sudo apt-get --force-yes --yes install vim
 	fi
 
+	# install cmake
+	PKG_OK=$(dpkg-query -W --showformat='${Status}\n' cmake|grep "install ok installed")
+	if [ "" == "$PKG_OK" ]; then
+  		sudo apt-get --force-yes --yes install cmake 
+	fi
+	
+	# install python
+	PKG_OK=$(dpkg-query -W --showformat='${Status}\n' python|grep "install ok installed")
+	if [ "" == "$PKG_OK" ]; then
+  		sudo apt-get --force-yes --yes install python
+	fi
+	
+	# install python-dev
+	PKG_OK=$(dpkg-query -W --showformat='${Status}\n' python-dev|grep "install ok installed")
+	if [ "" == "$PKG_OK" ]; then
+  		sudo apt-get --force-yes --yes install python-dev
+	fi
+
 	# install vundle
 	git.clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+	
+	# install plugins
+	vim +PluginInstall +qall -u vimrc
+
+	# install YouCompleteMe
+	if [ -f "$HOME/.vim/bundle/youcompleteme/install.py" ]; then
+		"$HOME/.vim/bundle/youcompleteme/install.py"
+	fi
 }
 
 pkg.link() {
-	# link 
 	fs.link_rfile vimrc $HOME/.vimrc
-	
-	# install plugins
-	vim +PluginInstall +qall
 }
 
 pkg.uninstall() {
